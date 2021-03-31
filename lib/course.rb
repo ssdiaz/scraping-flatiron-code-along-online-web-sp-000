5a689dbfcb462ad1c911107d39f940e6bc1a88a3
Sam D <samidiaz3@gmail.com>
2021-03-31 21:23:59 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,4 +1,19 @@
 class Course
   attr_accessor :title, :schedule, :description
+
+  @@aal = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
 end
 # rspec spec/course_spec.rb

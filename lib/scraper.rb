689003050576baa4e90f63204c2f2baf3078f414
Sam D <samidiaz3@gmail.com>
2021-03-31 21:37:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,21 +5,29 @@ require_relative './course.rb'
 
 class Scraper
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
   end
 
 
 end
 # rspec spec/scraper_spec.rb
+
+
+
+
+
+
+
+
+# def print_courses
+#   self.make_courses
+#   Course.all.each do |course|
+#     if course.title && course.title != ""
+#       puts "Title: #{course.title}"
+#       puts "  Schedule: #{course.schedule}"
+#       puts "  Description: #{course.description}"
+#     end
+#   end
+# end
